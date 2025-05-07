f704a0e82fd83eae779ad84bc6d6d8492b717ca5
millenavn <millenav.nunes08@gmail.com>
2025-05-07 17:59:19 -0300

    fix: verificação de JWT.

--- backend/src/swagger.js
+++ backend/src/swagger.js
@@ -1,38 +1,24 @@
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
-const path = require('path');
+const path = require("path");
 
 const swaggerOptions = {
   definition: {
-    openapi: '3.0.0',
+    openapi: "3.0.0",
     info: {
-      title: 'Documentação Swagger da API do V.I.D.A.',
-      version: '1.0.0',
-      description: 'Esta é a documentação Swagger da API do projeto V.I.D.A. (Vetor Inteligente de Decisão Assistida). Aqui você encontrará todas as rotas disponíveis, seus parâmetros, respostas e exemplos de uso.',
+      title: "Documentação Swagger da API do V.I.D.A.",
+      version: "1.0.0",
+      description:
+        "Esta é a documentação Swagger da API do projeto V.I.D.A. (Vetor Inteligente de Decisão Assistida). Aqui você encontrará todas as rotas disponíveis, seus parâmetros, respostas e exemplos de uso.",
     },
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT', // opcional, mas deixa claro que é JWT
-        },
-      },
-    },
-    security: [
-      {
-        bearerAuth: [],
-      },
-    ],
   },
-  apis: [path.join(__dirname, '/routes/*.js')],
+  apis: [path.join(__dirname, "/routes/*.js")],
 };
 
 const swaggerSpec = swaggerJsDoc(swaggerOptions);
 
-
 function setupSwagger(app) {
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 }
 
 module.exports = setupSwagger;
