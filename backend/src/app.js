48d453e8e759639e1d45606704ac9a1d0ec186ca
Andrei <andreiolicar7@gmail.com>
2025-07-02 11:40:54 -0300

    fix: correção do cors.

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(cors({
     origin: 'http://localhost:5173',
-    methods: ['GET', 'PATCH', 'POST', 'PUT', 'DELETE'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true
 }));
 app.use(express.json());
